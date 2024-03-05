b25a87f9464dd8385930a1a57f693a60ce0937e3
AidenRazack <aiden.razack25@ycdsbk12.ca>
2024-03-05 15:01:51 -0500

    done

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,62 @@
 import processing.core.PApplet;
-
+ /**
+  * A program that draws a human face that can adapt to any window size and is generated randomly with a changing background
+  * @author: A. Razack
+  */
 public class Sketch extends PApplet {
-	
+
+  // Define variables
+	int x;
+  int y;
 	
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
+
 	// put your size call here
     size(400, 400);
-  }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
-  public void setup() {
-    background(210, 255, 173);
-  }
-
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // Random x and y coordinate within the canvas width and height
+    x = (int) random(width);
+    y = (int) random(height);
   }
   
-  // define other methods down here.
+  /**
+  * Called repeatedly, anything drawn to the screen goes here
+  */
+  public void draw() {
+  
+    // Change background color based on the position of the face
+    if (x < width/2 && y < height/2) {
+      background(255, 204, 153);
+    } else if (x >= width/2 && y < height/2) {
+      background(153, 204, 255); 
+    } else if (x < width/2 && y >= height/2) {
+      background(255, 153, 204); 
+    } else {
+      background(153, 255, 204); 
+    }
+
+    // Draw head
+    fill(255, 204, 153); 
+    ellipse(x, y, 200, 200); 
+
+    // Draw eyes
+    fill(255); 
+    ellipse(x - 30, y - 30, 40, 40); 
+    ellipse(x + 30, y - 30, 40, 40); 
+
+    // Draw pupils
+    fill(0); 
+    ellipse(x - 30, y - 30, 20, 20); 
+    ellipse(x + 30, y - 30, 20, 20); 
+
+    // Draw nose
+    fill(255, 102, 102);
+    triangle(x, y - 10, x - 10, y + 20, x + 10, y + 20); 
+
+    // Draw mouth
+    arc(x, y + 40, 100, 80, 0, PI); 
+  }
 }
